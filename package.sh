aabd9184643fda9c22dcf14a5019c85767259566
Zach Curtis <zc@wayin.com>
2012-10-15 09:53:21 -0600

    Added package script

---
+++ package.sh
@@ -0,0 +1,11 @@
+# Script to package both extensions
+# By: Zach Curtis - Wayin, Inc
+
+TMPFOLDER=PATH TO MY LOCAL JOOMLA tmp FOLDER
+rm com_allplayers_auth-v1.0.zip 
+zip -r --exclude=*.git* --exclude=*.DS_Store* com_allplayers_auth-v1.0.zip ./com_allplayers_auth
+
+rm plugin_allplayers_auth-v1.0.zip
+zip -r --exclude=*.git* --exclude=*.DS_Store* plugin_allplayers_auth-v1.0.zip ./plugin_allplayers_auth
+
+#mv ./*.zip $TMPFOLDER
